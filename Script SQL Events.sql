775c376a0bb600e36ef47f4348ed9e71c7fbdaae
OlivierDeVos <37544964+OlivierDeVos@users.noreply.github.com>
2018-04-16 14:01:17 +0200

    Delete Script SQL Events.sql

--- Script SQL Events.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO 'events' ('name','date','description','URL','location','price') VALUES
-('Barbecue 12 avril','12/04/2018','Le premier barbecue de la saison, afin de faire connaissance avec les nouveaux membres','https://www.google.com/intl/fr/drive/Barbecue_12/4','41°24'12,2"N 2°10'26,5"E'),
-('Anniversaire Jean Dupont 16 avril','16/04/2018','41°24'12,2"N 2°10'26,5"E',17),
